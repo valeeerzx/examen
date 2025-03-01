c1a79302a16a97951fa66f6c42ff56e04523aa6b
valeeerzx <kuvaleria78@gmail.com>
2025-02-28 21:40:29 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function contarPalabras(frase) {
-    let palabras = frase.split(" ").filter(palabra => palabra.length > 0);
+    let palabras = frase.split(/\s+/).filter(palabra => palabra.length > 0);
     return palabras.length;
 }
 
 function contarPalabrasEnFrase() {
     let frase = document.getElementById("inputText").value;
     let numeroDePalabras = contarPalabras(frase); 
-
 
     let resultadoDiv = document.getElementById("resultado");
     resultadoDiv.textContent = "La frase tiene " + numeroDePalabras + " palabra(s).";
